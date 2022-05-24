f3cf1f7776eef148c39b7ec98195f77d0abed745
Lukasz Sosnowy <lukasz.sosnowy.ctr@sabre.com>
2022-05-24 10:02:36 +0200

    basic operations

--- src/main/java/basics/Basics.java
+++ src/main/java/basics/Basics.java
@@ -1,5 +1,31 @@
 package basics;
 
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+
+
 public class Basics {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+
+            BrowserType.LaunchOptions lp = new BrowserType.LaunchOptions();
+            lp.setChannel("chrome");
+            lp.setHeadless(false);
+            Browser browser = playwright.chromium().launch(lp);
+            Page page = browser.newPage();
+            page.navigate("https://www.amazon.com");
+
+            String title = page.title();
+            System.out.println(title);
+
+            String url = page.url();
+            System.out.println(url);
+
+            browser.close();
+        }
+    }
 
 }
