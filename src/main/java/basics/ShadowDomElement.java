71c2c6abd6d8b824e1d1f5f1f3728d39c00161ad
lukasowy <lukasowy@gmail.com>
2022-05-27 15:36:45 +0200

    Explore methods to interact with the ShadowDom

--- src/main/java/basics/ShadowDomElement.java
+++ src/main/java/basics/ShadowDomElement.java
@@ -3,16 +3,36 @@ package basics;
 import com.microsoft.playwright.*;
 
 public class ShadowDomElement {
+    private static final String URL_ADDRESS_1 = "http://bookS-pwakit.appspot.com/";
+    private static final String URL_ADDRESS_2 = "https://selectorshub.com/xpath-practice-page/";
+
     public static void main(String[] args) {
 
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 
-            BrowserContext browserContext = browser.newContext();
-            Page page = browserContext.newPage();
+            //Page -- DOM --> Shadow DOM --> elements
+            //Page -- DOM --> iFrame --> Shadow DOM --> elements
 
-            page.close();
-            browserContext.close();
+            BrowserContext browserContext1 = browser.newContext();
+            Page page1 = browserContext1.newPage();
+
+            page1.navigate(URL_ADDRESS_1);
+            page1.locator("book-app[apptitle='BOOKS'] #input").fill("Maniek");
+            String textContent = page1.locator("book-app[apptitle='BOOKS'] .books-desc").textContent();
+            System.out.println(textContent);
+
+            BrowserContext browserContext2 = browser.newContext();
+            Page page2 = browserContext2.newPage();
+
+            page2.navigate(URL_ADDRESS_2);
+            page2.frameLocator("#pact").locator("div#snacktime #tea").fill("Elo Elo");
+
+            page1.close();
+            browserContext1.close();
+
+            page2.close();
+            browserContext2.close();
         }
     }
 }
