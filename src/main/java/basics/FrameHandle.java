8939789f1ca84332cee4fb8a1c4f4f16642c4d39
lukasowy <lukasowy@gmail.com>
2022-05-27 12:33:45 +0200

    Explore methods to interact with the frame

--- src/main/java/basics/FrameHandle.java
+++ src/main/java/basics/FrameHandle.java
@@ -4,14 +4,25 @@ import com.microsoft.playwright.*;
 
 public class FrameHandle {
 
+    private static final String URL_ADDRESS = "http://www.londonfreelance.org/courses/frames/index.html";
+
     public static void main(String[] args) {
+
 
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 
-            BrowserContext browserContext1 = browser.newContext();
+            BrowserContext browserContext = browser.newContext();
 
-            browserContext1.close();
+            Page page = browserContext.newPage();
+            page.navigate(URL_ADDRESS);
+
+            FrameLocator frameLocator = page.frameLocator("//frame[@name='main']");
+            String h2 = frameLocator.locator("h2").textContent();
+            System.out.println(h2);
+
+            page.close();
+            browserContext.close();
         }
     }
 }
