fd5c3b77568ddf6dcc371718a9bf871af01b4e19
lukasowy <lukasowy@gmail.com>
2022-06-01 14:52:39 +0200

    Explore CSS and XPath separated by commas

--- src/main/java/basics/CommaSelectors.java
+++ src/main/java/basics/CommaSelectors.java
@@ -1,7 +1,28 @@
 package basics;
 
-public class CommaSelectors {
-    public static void main(String[] args) {
+import com.microsoft.playwright.*;
 
+public class CommaSelectors {
+
+    private static final String URL_ADDRESS = "https://academy.naveenautomationlabs.com/";
+
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions()
+                    .setHeadless(false));
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+
+            page.navigate(URL_ADDRESS);
+
+            Locator locator = page.locator("span:has-text('SignIn'), " +
+                    "span:has-text('LogIn'), " +
+                    "span:has-text('Login'), " +
+                    "span:has-text(Signin)");
+
+
+            page.close();
+            context.close();
+        }
     }
 }
