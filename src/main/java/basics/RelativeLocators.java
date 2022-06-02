202b04d3e355cdfda22680e0c92fe1c8a253392e
lukasowy <lukasowy@gmail.com>
2022-06-02 08:47:04 +0200

    Extract a method to select a user

--- src/main/java/basics/RelativeLocators.java
+++ src/main/java/basics/RelativeLocators.java
@@ -5,6 +5,7 @@ import com.microsoft.playwright.*;
 public class RelativeLocators {
 
     private static final String URL_ADDRESS = "https://selectorshub.com/xpath-practice-page/";
+    private static Page page;
 
     public static void main(String[] args) {
 
@@ -12,13 +13,18 @@ public class RelativeLocators {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 
             BrowserContext browserContext = browser.newContext();
-            Page page = browserContext.newPage();
+            page = browserContext.newPage();
 
             page.navigate(URL_ADDRESS);
-            page.locator("input[type='checkbox']:left-of(:text('Joe.Root'))").first().click();
+            selectUser("Joe.Root");
+            selectUser("Jasmine.Morgan");
 
             page.close();
             browserContext.close();
         }
+    }
+
+    public static void selectUser(String userName) {
+        page.locator("input[type='checkbox']:left-of(:text('" + userName + "'))").first().click();
     }
 }
