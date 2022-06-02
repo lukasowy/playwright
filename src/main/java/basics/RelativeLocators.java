272be8ab821d229aadca1ea0fd868dd61fb46c53
lukasowy <lukasowy@gmail.com>
2022-06-02 08:43:55 +0200

    Explore relative locators - left-of

--- src/main/java/basics/RelativeLocators.java
+++ src/main/java/basics/RelativeLocators.java
@@ -4,14 +4,18 @@ import com.microsoft.playwright.*;
 
 public class RelativeLocators {
 
+    private static final String URL_ADDRESS = "https://selectorshub.com/xpath-practice-page/";
+
     public static void main(String[] args) {
-        
+
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 
             BrowserContext browserContext = browser.newContext();
             Page page = browserContext.newPage();
 
+            page.navigate(URL_ADDRESS);
+            page.locator("input[type='checkbox']:left-of(:text('Joe.Root'))").first().click();
 
             page.close();
             browserContext.close();
