1625782dc1609cc856cdc528fcaff75e1d3f469d
lukasowy <lukasowy@gmail.com>
2022-06-04 11:21:32 +0200

    Fill field near to an element

--- src/main/java/basics/NearLocatorRelative.java
+++ src/main/java/basics/NearLocatorRelative.java
@@ -4,12 +4,16 @@ import com.microsoft.playwright.*;
 
 public class NearLocatorRelative {
 
+    private static final String URL_ADDRESS = "http://automationpractice.com/index.php";
+
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 
             BrowserContext browserContext = browser.newContext();
             Page page = browserContext.newPage();
+            page.navigate(URL_ADDRESS);
+            page.locator("input:near(:text('Newsletter'))").first().fill("maniek@gmail.com");
 
 
             page.close();
