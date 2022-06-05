25e514048835f80efa2003e278bdcd5952d3b44f
lukasowy <lukasowy@gmail.com>
2022-06-05 21:46:46 +0200

    Practice with Dynamic WebTable

--- src/main/java/basics/ScopeLocators.java
+++ src/main/java/basics/ScopeLocators.java
@@ -3,6 +3,7 @@ package basics;
 import com.microsoft.playwright.*;
 
 public class ScopeLocators {
+    private static final String URL_ADDRESS = "https://datatables.net/extensions/select/examples/initialisation/checkbox.html";
 
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
@@ -10,6 +11,12 @@ public class ScopeLocators {
 
             BrowserContext browserContext = browser.newContext();
             Page page = browserContext.newPage();
+            page.navigate(URL_ADDRESS);
+
+            Locator row = page.locator("table#example tr");
+            row.locator(":scope", new Locator.LocatorOptions().setHasText("Ashton Cox")).locator(".select-checkbox").click();
+
+            row.locator(":scope").allInnerTexts().forEach(System.out::println);
 
             page.close();
             browserContext.close();
