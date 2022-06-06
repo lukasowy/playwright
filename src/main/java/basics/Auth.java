050e399c9b9bebfcbbd09903becec966d9b1419c
lukasowy <lukasowy@gmail.com>
2022-06-06 22:00:43 +0200

    Login to the page

--- src/main/java/basics/Auth.java
+++ src/main/java/basics/Auth.java
@@ -2,6 +2,8 @@ package basics;
 
 import com.microsoft.playwright.*;
 
+import java.nio.file.Paths;
+
 public class Auth {
 
     public static void main(String[] args) {
@@ -10,6 +12,14 @@ public class Auth {
 
             BrowserContext browserContext = browser.newContext();
             Page page = browserContext.newPage();
+
+            page.navigate("http://automationpractice.com/");
+            page.click("a:text('Sign in')");
+            page.fill("#email", "maniek@gmai.com");
+            page.fill("#passwd", "maniek123");
+            page.click("#SubmitLogin");
+
+            browserContext.storageState(new BrowserContext.StorageStateOptions().setPath(Paths.get("application.json")));
 
             page.close();
             browserContext.close();
