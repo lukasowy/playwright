00c1c19c7e039541cc24d93646699d285c40c6d0
lukasowy <lukasowy@gmail.com>
2022-06-06 22:02:28 +0200

    Create NewAuth.java

---
+++ src/main/java/basics/NewAuth.java
@@ -0,0 +1,17 @@
+package basics;
+
+import com.microsoft.playwright.*;
+
+public class NewAuth {
+    public static void main(String[] args) {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+            BrowserContext browserContext = browser.newContext();
+            Page page = browserContext.newPage();
+
+            page.close();
+            browserContext.close();
+        }
+    }
+}
