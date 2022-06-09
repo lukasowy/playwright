ea9b15854594987af9e3e3879a4fbc0e5cb030bc
lukasowy <lukasowy@gmail.com>
2022-06-09 21:34:44 +0200

    Download a file

--- src/main/java/basics/DownloadFile.java
+++ src/main/java/basics/DownloadFile.java
@@ -2,6 +2,8 @@ package basics;
 
 import com.microsoft.playwright.*;
 
+import java.nio.file.Paths;
+
 public class DownloadFile {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
@@ -9,6 +11,21 @@ public class DownloadFile {
 
             BrowserContext browserContext = browser.newContext();
             Page page = browserContext.newPage();
+            page.navigate("https://chromedriver.storage.googleapis.com/index.html?path=102.0.5005.27/");
+
+            Download download = page.waitForDownload(() -> {
+                page.click("a:text('chromedriver_mac64.zip')");
+            });
+            
+            System.out.println(download.failure());
+
+            System.out.println(download.url());
+
+            String path = download.path().toString();
+            System.out.println(path);
+
+            download.saveAs(Paths.get("Naveen_chrome.zip"));
+            System.out.println(download.suggestedFilename());
 
             page.close();
             browserContext.close();
