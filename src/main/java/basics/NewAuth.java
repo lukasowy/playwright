a87ae2b01801524d2c11dd1d6c70e4ff06c9b310
lukasowy <lukasowy@gmail.com>
2022-06-10 09:24:15 +0200

    Use saved login data

--- src/main/java/basics/NewAuth.java
+++ src/main/java/basics/NewAuth.java
@@ -2,12 +2,16 @@ package basics;
 
 import com.microsoft.playwright.*;
 
+import java.nio.file.Paths;
+
 public class NewAuth {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
 
-            BrowserContext browserContext = browser.newContext();
+            BrowserContext browserContext =
+                    browser.newContext(new Browser.NewContextOptions().setStorageStatePath(Paths.get("application.json")));
+
             Page page = browserContext.newPage();
 
             page.close();
