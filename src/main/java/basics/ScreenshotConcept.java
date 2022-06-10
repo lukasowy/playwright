17f0a0fef9c9822329d473fd0280a286758f5956
lukasowy <lukasowy@gmail.com>
2022-06-10 09:28:53 +0200

    Make a screenshot and save

--- src/main/java/basics/ScreenshotConcept.java
+++ src/main/java/basics/ScreenshotConcept.java
@@ -2,6 +2,8 @@ package basics;
 
 import com.microsoft.playwright.*;
 
+import java.nio.file.Paths;
+
 public class ScreenshotConcept {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
@@ -9,7 +11,12 @@ public class ScreenshotConcept {
 
             BrowserContext browserContext = browser.newContext();
             Page page = browserContext.newPage();
+            page.navigate("https://naveenautomationlabs.com/opencart/");
 
+            page.screenshot(new Page.ScreenshotOptions()
+                    .setPath(Paths.get("screenshotpage.png"))
+                    .setFullPage(true));
+            
             page.close();
             browserContext.close();
         }
